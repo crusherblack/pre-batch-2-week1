c356f27ec1fbe64452c0ac80394fab353ae42dec
fadhil <fadhildarma13@gmail.com>
2021-03-24 14:09:41 +0700

    init

---
+++ app.js
@@ -0,0 +1,31 @@
+document.getElementById("testing").innerHTML = "INI DARI JAVASCRIPT";
+
+//Primitive Data Types
+//var
+//let
+//const
+
+var name = "Samsul Rijal"; //variable dengan type var dan type datanya adalah string
+var semester = 6; //number
+var isFreelance = true; //boolean
+var bilangBerkoma = 9.5; //number
+
+console.log(typeof name);
+console.log(typeof semester);
+console.log(typeof isFreelance);
+console.log(typeof bilangBerkoma);
+
+var bilangan1 = 100; //number
+var bilangan2 = 2000; //number
+var penjumlahan = bilangan1 + bilangan2; // 2100
+var apakahLebihKecil = bilangan1 < bilangan2; //true
+
+console.log(penjumlahan);
+console.log(apakahLebihKecil);
+
+var hello; //undefined
+var kosong = null; //null
+console.log(hello);
+console.log(kosong);
+
+//Non Primitive Data Types
